04ead707927e88bc54727c93e369afe4c898d921
Rahul Pai <rahulpai123sap@gmail.com>
2020-10-16 12:30:08 +0530

    Fixed singel session cookie for Server & Client
    - Added session to middleware.
    - Removed redundant codes from index.ts.
    - Added cors: accept headers, sameorigin, credentials.

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,43 @@
+import generateEnv from "./config/config";
 import express, { json } from "express";
-import session from "express-session";
-import MongoDB_ConnectSession from "connect-mongodb-session";
-import compression from "compression";
 import cors from "cors";
 import helmet from "helmet";
-import generateEnv from "./config/config";
+import compression from "compression";
+import session_middleware from "./middleware/session";
 import auth_middleware from "./middleware/auth";
 import grpahql_middleware from "./middleware/graphql";
 import route_middleware from "./middleware/route";
 import { Status } from "./utility/Interface";
-import oServe_Utility from "./dev/UtilityClass";
 
 const PORT = generateEnv().PORT;
-const MONGODB_URI = generateEnv().MongoDB.MONGODB_URI;
 const app = express();
 const cors_option = {
-  origin: "*",
+  origin: ["http://localhost:3000"],
   exposedHeaders: ["x-auth-token", "x-csrf-token"],
+  credentials: true,
 };
-const MongoDB_SessionStore = MongoDB_ConnectSession(session);
-const session_store = new MongoDB_SessionStore(
-  {
-    uri: `${MONGODB_URI}`,
-    collection: "Session",
-    connectionOptions: {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-  },
-  (error) => {
-    if (error) {
-      console.log(`${Status.MongoDB_session_error}:${error}`);
-    }
-  }
-);
 
-session_store.on("error", (error) => {
-  console.log(`${Status.MongoDB_session_error}:${error}`);
-  return error;
-});
-
+//Set to app
 app.set("trust proxy", 1);
+
+//Std Middelwares
 app.use(json());
 app.use(cors(cors_option));
 app.use(helmet());
 app.use(compression());
 
-app.use(
-  session({
-    secret: oServe_Utility.GenerateSecretKey(),
-    resave: false,
-    saveUninitialized: false,
-    store: session_store,
-    cookie: {
-      maxAge: 180000,
-      secure: false,
-      httpOnly: true,
-    },
-  })
-);
-
+//Custom Middelwares
+app.use(session_middleware);
 app.use(auth_middleware);
 app.use(grpahql_middleware);
 app.use(route_middleware);
 
+//Server Listener
 const server = app.listen(PORT, () => {
   console.log(`${Status.ListeningonPort}: ${PORT}`);
 });
 
+//Server graceful exit
 process.on("SIGTERM", () => {
   console.log(Status.Closing_http_server);
   server.close(() => {
---
+++ src/middleware/session.ts
@@ -0,0 +1,51 @@
+import session from "express-session";
+import MongoDB_ConnectSession from "connect-mongodb-session";
+import oServe_Utility from "../dev/UtilityClass";
+import express from "express";
+import generateEnv from "../config/config";
+import { Status } from "../utility/Interface";
+
+const app = express();
+const MONGODB_URI = generateEnv().MongoDB.MONGODB_URI;
+const MongoDB_SessionStore = MongoDB_ConnectSession(session);
+const session_store = new MongoDB_SessionStore(
+  {
+    uri: `${MONGODB_URI}`,
+    collection: "Session",
+    connectionOptions: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  },
+  (error) => {
+    if (error) {
+      console.log(`${Status.MongoDB_session_error}:${error}`);
+    }
+  }
+);
+
+/**
+ * Middleware - GrpahQL Http
+ * @description
+ * Middleware defining GraphQL Http and configurations
+ */
+const session_middleware = app.use(
+  session({
+    secret: oServe_Utility.GenerateSecretKey(),
+    resave: false,
+    saveUninitialized: false,
+    store: session_store,
+    cookie: {
+      maxAge: 1800000,
+      secure: false,
+      httpOnly: true,
+      sameSite: "strict",
+    },
+  })
+);
+
+session_store.on("error", (error) => {
+  return error;
+});
+
+export default session_middleware;
