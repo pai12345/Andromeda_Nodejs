66e3f8c1e083812b156567fd8ec96c560c3912e9
Rahul Pai <rahulpai123sap@gmail.com>
2020-11-03 23:40:03 +0530

    Modified - route middleware.

--- src/middleware/route.ts
+++ src/middleware/route.ts
@@ -1,21 +1,17 @@
-import express from "express";
+import { RequestHandler } from "express";
 import { Status } from "../utility/Interface";
-
-const app = express();
 
 /**
  * Middleware - Route Validation
  * @description
  * Middleware to validate routes
  */
-export const route_middleware = app.use(async (_req, res, next) => {
+const route_middleware: RequestHandler = async (_req, res) => {
   try {
     res.status(Status.NotFound).send(Status.PageNotFoundTitlte);
-    next();
   } catch (error) {
     res.status(error.esponse.status).send(error);
-    next();
   }
-});
+};
 
 export default route_middleware;
